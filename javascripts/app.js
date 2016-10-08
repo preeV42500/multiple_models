ff841e5d48f6a475439ac295624797719c5a6c0b
Preeti Viswanathan <vis.preeti@gmail.com>
2016-10-07 19:48:45 -0700

    Replace from-scratch collection management code with a Backbone collection

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,56 +1,57 @@
-var ItemModel = Backbone.Model.extend({ // item model constructor
-  idAttribute: "id"
-});
-
-var Items = { // represents collection of models and methods related to the collection
+var App = {
   $body: $("tbody"),
-  collection: [],
-  create: function(item_data) {
-    item_data.id = this.collection.length + 1;
-    var item = new ItemModel(item_data); // use item data to create new item model
-    this.collection.push(item); // add newly created model to the collection
-
-    return item;
-  },
+  template: Handlebars.compile($("#items").html()),
   render: function() { // render collection of models to the page
-    this.$body.html(template({
-      items: this.collection
+    this.$body.html(this.template({
+      items: this.Items.models
     }));
   },
-  seedCollection: function() { // create a new model for each obj in items_json array
-    items_json.forEach(this.create.bind(this));
-  },
-  sortBy: function(prop) { // sorts collection based on specified property and re-renders it
-    this.collection = _(this.collection).sortBy(function(model) {
-      return model.attributes[prop];
-    });
-    this.render();
-  },
-  empty: function() { // delete models from view and from collection
-    this.collection = [];
-    this.render();
-  },
-  remove: function(e) {
+  removeItem: function(e) {
     e.preventDefault();
-    var $e = $(e.target),
-        model = _(this.collection).findWhere({ id: +$e.attr("data-id") });
-
-    this.collection = _(this.collection).without(model); // reset collection to array without the model
-    this.render();
+    var model = this.Items.get(+$(e.target).attr("data-id"));
+    this.Items.remove(model);
   },
   bind: function() {
-    this.$body.on("click", "a", this.remove.bind(this)); // delegate click event on delete anchor to tbody element
+    this.$body.on("click", "a", this.removeItem.bind(this)); // delegate click event on delete anchor to tbody element
   },
   init: function() {
-    this.seedCollection();
+    this.Items = new ItemsCollection(items_json); // create new collection seeded with items_json data
+    this.Items.sortByName(); // sort by name
     this.render();
     this.bind();
   }
-
 };
 
-// compile Handlebars template
-var template = Handlebars.compile($("#items").html());
+var ItemModel = Backbone.Model.extend({ // item model constructor
+  idAttribute: "id",
+  initialize: function() {
+    this.collection.incrementID();
+    this.set("id", this.collection.last_id);
+  }
+});
+
+// Create collection constructor
+var ItemsCollection = Backbone.Collection.extend({
+  last_id: 0,
+  model: ItemModel,
+  incrementID: function() {
+    this.last_id++;
+  },
+  sortBy: function(prop) { // sorts collection based on specified property and re-renders it
+    this.models = _(this.models).sortBy(function(model) {
+      return model.attributes[prop];
+    });
+    App.render();
+  },
+  sortByName: function() {
+    this.sortBy("name");
+  },
+  initialize: function() {
+    this.on("remove reset", App.render.bind(App)); // bind remove and reset event listeners to collection to re-render it to page
+    this.on("add", this.sortByName);
+  }
+});
+
 // register partial
 Handlebars.registerPartial("item", $("#item").html());
 
@@ -63,19 +64,19 @@ $("form").on("submit", function(e) {
     attrs[input.name] = input.value; // store form input names and corresponding values on attrs obj
   });
 
-  item = Items.create(attrs);
-  Items.$body.append(Handlebars.partials.item(item.toJSON())); // append new item to end of table body
+  item = App.Items.add(attrs);
+
   this.reset();
 });
 
 $("th").on("click", function() { // bind click event to table headings to sort table by heading property
   var prop= $(this).attr("data-prop");
-  Items.sortBy(prop);
+  App.Items.sortBy(prop);
 });
 
 $("p a").on("click", function(e) { // bind click event to 'Delete All' link
   e.preventDefault();
-  Items.empty();
+  App.Items.reset();
 });
 
-Items.init();
+App.init();
